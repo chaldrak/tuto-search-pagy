76592ba1035a9a64584f6fabab575380017148d8
Chaldrak <chaldrakus@gmail.com>
2021-08-30 02:11:03 +0100

    pagy

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,8 @@
 class EmployeesController < ApplicationController
+  include Pagy::Backend
+
   def index
-    @employees = Employee.all.order(name: :asc)
-    @employees = Employee.where(name: params[:q]) if params[:q]
+    @pagy, @employees = pagy(Employee.all.order(name: :asc), page: params[:page], items: 10)
+    @pagy, @employees = pagy(Employee.where(name: params[:q]), page: params[:page], items: 10) if params[:q]
   end
 end
